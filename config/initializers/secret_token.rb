42a1f8fb6dfcc7ba6ed31f23c3df4a294f69e89b
juanpabloe <juannpablo@gmail.com>
2011-09-10 21:39:16 -0500

    Inicio de app con autenticacion from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Extrapoints::Application.config.secret_token = '19ed872bcd66a4273fba639111f923bf66992c73ad6ffa3c938221f6a9369a7fc68ae425ac9d8b536ae387bb3c43a88c5cc1f4222d1a97eeabc149f1cd0716ab'
