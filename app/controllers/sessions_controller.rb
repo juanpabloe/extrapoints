5328e269bbd2a131ad793c51e98facbee3cb16fe
juanpabloe <juannpablo@gmail.com>
2011-09-17 14:30:25 -0500

    Fixed redirection bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
-    redirect_to users_path if current_user
+    redirect_to after_sign_in_path(current_user) if current_user
   end
 
   def create
     user = User.authenticate(params[:username], params[:password])
     ws_login = User.login_user(params[:username], params[:password])
     if user and ws_login[:user_id].to_i >= 1
-      assign_session_credientials(user, ws_login) 
-      if user.student?
-        redirect_to students_path
-      elsif user.teacher?
-        redirect_to teachers_path
-      else
-        redirect_to users_path
-      end
+      assign_session_credentials(user, ws_login) 
+      redirect_to after_sign_in_path(user)
     else
       User.logout(ws_login[:user_id])
       render "new"
@@ -24,14 +18,14 @@ class SessionsController < ApplicationController
 
   def destroy
     if User.logout(session[:user_id_ws])
-      destroy_session_credientials
+      destroy_session_credentials
       redirect_to root_url, :notice => "Logged Out!"
     else
       flash.now.alert = "Can't logout"
     end
   end
 
-  def assign_session_credientials(user, ws_login)
+  def assign_session_credentials(user, ws_login)
       session[:user_id] = user.id
       session[:user_id_ws] = ws_login[:user_id]
   end
@@ -39,6 +33,16 @@ class SessionsController < ApplicationController
   def destroy_session_credentials
       session[:user_id] = nil
       session[:user_id_ws] = nil
+  end
+
+  def after_sign_in_path(user)
+      if user.student?
+        students_path
+      elsif user.teacher?
+        teachers_path
+      else
+        users_path
+      end
   end
 
 end
