9f8016f832f7a838abfbe1fd50154900efdbb3ff
juanpabloe <juannpablo@gmail.com>
2011-10-25 20:20:37 -0500

    Changed seeds for teachers first

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,15 @@ require 'csv'
 puts "Hola"
 
 User.delete_all
-puts "Creando estudiantes"
-CSV.open("ExtraPoints_Users.csv", "r").each do |row|
-  Student.create!(:username => row[3], :password => row[4], :first_name => row[0], :last_name => row[1], :dob => row[2], :pin => row[5], :points => row[6])
-end
 
 puts "Creando profesores"
 CSV.open("ExtraPoints_Teachers.csv", "r").each do |row|
   Teacher.create!(:username => row[3], :password => row[4], :first_name => row[0], :last_name => row[1], :dob => row[2], :pin => row[5], :points => row[6])
+end
+
+puts "Creando estudiantes"
+CSV.open("ExtraPoints_Users.csv", "r").each do |row|
+  Student.create!(:username => row[3], :password => row[4], :first_name => row[0], :last_name => row[1], :dob => row[2], :pin => row[5], :points => row[6])
 end
 
 puts "Limpiando nombres y apellidos"
